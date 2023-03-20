73e1c9ec9406bdc3d21071560f55c346e125e551
unknown <orc0926@dlsud.edu.ph>
2023-03-20 22:47:54 +0800

    230320

---
+++ indexActivity.js
@@ -0,0 +1,22 @@
+console.log("Hello, Welcome to De La Salle University!");
+
+let firstName = 'Rafael Archie';
+let lastName = 'Ogot';
+let age = 25;
+let hobbies = ["reading", "playing", "painting", "travelling", "playing volleyball"];
+let homeAddress = {
+	houseNumber: "Blk 61 L2&4",
+	street: "Ashcroft St.",
+	villageOrSubdivision: "Governor's Subdivision",
+	barangay: "Biclatan",
+	city: "Cavite",
+	province: "Metro Manila",
+	zipcode: 4107
+};
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(hobbies);
+console.log(homeAddress);
+
